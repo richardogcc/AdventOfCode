21f8e47ffd1d8241731a9c1e1eb8a48c508fd4cd
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2020-12-15 20:04:48 +0100

    Painful Day03

--- 2020/Day03/main.py
+++ 2020/Day03/main.py
@@ -3,3 +3,32 @@
 
 def readFromFile(pathFile):
     return (open(pathFile, 'r').read()).split('\n')
+
+def counter(rawInput):
+    down = 0
+    right = 0
+    trees = 0
+
+    for line in rawInput:
+        if right > len(line):
+            break
+
+        if line[right] == '#':
+            trees += 1
+        
+        '''-------------------------'''
+        line = list(line)
+        if line[right] == '.':
+            line[right] = 'O'
+        else:
+            line[right] = 'X'
+
+        line = ''.join(line)
+        print(line)
+        '''-------------------------'''
+
+        down += 1
+        right += 3
+    print('\n\n\nDown: {}\nRight: {}\nTrees: {}'.format(down, right, trees))
+
+counter(readFromFile('input.txt'))
