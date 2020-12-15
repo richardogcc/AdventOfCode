4a3cccd3b9000ff482ceed8f97f906b5b9326d9a
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2020-12-15 23:41:19 +0100

    Day03 Finished

--- 2020/Day03/main.py
+++ 2020/Day03/main.py
@@ -4,31 +4,40 @@
 def readFromFile(pathFile):
     return (open(pathFile, 'r').read()).split('\n')
 
-def counter(rawInput):
+def counter(rawInput, in_down, in_right):
     down = 0
     right = 0
     trees = 0
 
+    forest = []
     for line in rawInput:
-        if right > len(line):
-            break
+        forest.append(list(line))
 
-        if line[right] == '#':
+    while down + 1 < len(forest):
+        down += in_down
+        right += in_right
+
+        if forest[down][right % len(forest[down])] == '#':
             trees += 1
-        
-        '''-------------------------'''
-        line = list(line)
-        if line[right] == '.':
-            line[right] = 'O'
-        else:
-            line[right] = 'X'
 
-        line = ''.join(line)
-        print(line)
-        '''-------------------------'''
+        '''-------Only GUI Result--------'''
+        # if forest[down][right % len(forest[down])] == '.':
+        #     forest[down][right % len(forest[down])] = 'O'
+        # else:
+        #     forest[down][right % len(forest[down])] = 'X'
+        # print('{}\tFila: {}, Columna: {}'.format(''.join(forest[down]), down, right % len(forest[down])))
+        '''-------------------------------'''
 
-        down += 1
-        right += 3
-    print('\n\n\nDown: {}\nRight: {}\nTrees: {}'.format(down, right, trees))
+    # print('\n\n\nDown: {}\nTrees: {}'.format(down, trees))
+    return trees
 
-counter(readFromFile('input.txt'))
+
+c1 = counter(readFromFile('input.txt'), 1, 3)
+print('Result parte one: ' + str(c1))
+
+c1 = c1 * counter(readFromFile('input.txt'), 1, 1)
+c1 = c1 * counter(readFromFile('input.txt'), 1, 5)
+c1 = c1 * counter(readFromFile('input.txt'), 1, 7)
+c1 = c1 * counter(readFromFile('input.txt'), 2, 1)
+
+print('Final result: ' + str(c1))
