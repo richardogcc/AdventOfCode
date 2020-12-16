6fc0edf2d36ae4eefeddcc2471c9fb05f7dc4846
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2020-12-16 17:50:34 +0100

    Refactoring Day04

--- 2020/Day04/main.py
+++ 2020/Day04/main.py
@@ -10,11 +10,12 @@ for i in readFromFile('input.txt'):
         i = ';'
     formated = (formated + ' ' + i).strip()
 
+
 def createObject(string):
     node = {}
 
     for i in string.split(' '):
-        if len(i.split(':')[:1]) > 0 and len(i.split(':')[1:]):
+        if len(i.split(':')[:1]) > 0 and len(i.split(':')[1:]) > 0:
             key = i.split(':')[:1][0]
             value = i.split(':')[1:][0]
             node[key] = value
@@ -27,16 +28,19 @@ def listOfObjects(string):
         created = createObject(i)
         if bool(created):
             lofobj.append(created)
+
     return lofobj
 
-isV = None
-validPassport = 0
-for entry in listOfObjects(formated):
-    v = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    isValid = v[0] in entry and v[1] in entry and v[2] in entry and v[3] in entry and v[4] in entry and v[5] in entry and v[6] in entry
+def validate():
+    validPassport = 0
 
-    if isValid:
-        validPassport += 1
+    for entry in listOfObjects(formated):
+        v = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+        isValid = v[0] in entry and v[1] in entry and v[2] in entry and v[3] in entry and v[4] in entry and v[5] in entry and v[6] in entry
 
-print(isV)
-print(validPassport)
+        if isValid:
+            validPassport += 1
+            
+    return validPassport
+
+print(validate())
