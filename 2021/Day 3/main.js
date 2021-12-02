d39cc2a73363af3e3316827cc95e0b39c37ccfa7
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2021-12-02 17:42:43 +0100

    Prepared Day 3 AOC

---
+++ 2021/Day 3/main.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const input = './input.txt';
+const fileRead = (fileName) => (fs.readFileSync(fileName)).toString();
+const inputLn = (fileRead(input)).split('\n');
+
+// Answer first part: 
+console.log();
+
+// Answer second part:
+console.log();
