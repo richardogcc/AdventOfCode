f825d37e8a2a4ffabc9e08af823524251728cf74
Richardo G.C. <19938775+richardogcc@users.noreply.github.com>
2024-12-05 09:14:18 +0100

    Crazy moment on refactor day 2

--- 2024/Day 2/main.js
+++ 2024/Day 2/main.js
@@ -6,25 +6,50 @@ const read = (fileRead(input)).split('\r\n')
 const print = (txt) => console.log(txt);
 
 // Part 1
-function isUpOrDown(arr) {
-    if(arr.every((value, index) => index === 0 || value > arr[index - 1])) return true;
-    arr.reverse()
-    return arr.every((value, index) => index === 0 || value > arr[index - 1]);
-}
+// function isUpOrDown(arr) {
+//     let increment = 1;
+//     if(arr.every((value, index) => index === 0 || value > arr[index - increment])) {
+//         return true;
+//     }
+//     arr.reverse()
+//     return arr.every((value, index) => index === 0 || value > arr[index - increment]);
+// }
 
-const safety = (arr) => {
+// const safety = (arr) => {
+//     for(let i = 0; i < arr.length; i++){
+//         const r = arr[i] < arr[i+1] ? arr[i+1] - arr[i] : arr[i] - arr[i+1];
+//         if(r < 1 || r > 3) return false;
+//     }
+
+//     return true;
+// }
+
+function safety(arr, calledOnce = false) {
+    console.log('Called once ', calledOnce);
+    console.log('Array ', arr.join(' '));
+
     for(let i = 0; i < arr.length; i++){
-        const r = arr[i] < arr[i+1] ? arr[i+1] - arr[i] : arr[i] - arr[i+1];
-        if(r < 1 || r > 3) return false;
+        if(!arr[i+1]) break;
+        console.log(`i: ${i} | Math.abs(${arr[i]} - ${arr[i+1]}) = ${Math.abs(arr[i] - arr[i+1])}`);
+        // const r = Math.abs(arr[i] - arr[i+1]);
+        const r = (arr[i] - arr[i+1]) < 0 ? (arr[i] - arr[i+1]) * -1 : arr[i] - arr[i+1];
+        console.log('R ', r);
+
+        if(r < 1 || r > 3 && calledOnce == false){
+            arr.splice(i+1, 1);
+            safety(arr, true);
+        }
+        if(r < 1 || r > 3) return [arr, false, `i: ${i} | Math.abs(${arr[i]} - ${arr[i+1]}) = ${r}`];
     }
 
-    return true;
+    console.log('Reached end');
+    return [arr, true];
 }
 
 const lvls = read.map(x => x.split(' ').map(Number))
-const a1 = lvls.filter(x => isUpOrDown(x) && safety(x)).length
+// const a1 = lvls.filter(x => /*isUpOrDown(x) && */safety(x)).length
+const a1 = lvls.map(x => /*isUpOrDown(x) && */safety(x))
 // Response 524
 print(a1)
 
 // Part 2
-
---
+++ 2024/Day 3/main.js
@@ -0,0 +1,30 @@
+import { readFileSync } from 'fs';
+
+const input = './debug.txt';
+const fileRead = (fileName) => (readFileSync(fileName)).toString();
+const read = (fileRead(input)).split('\r\n')
+const print = (txt) => console.log(txt);
+
+// Part 1
+function isUpOrDown(arr) {
+    let increment = 1;
+    if(arr.every((value, index) => index === 0 || value > arr[index - increment])) return true;
+    arr.reverse()
+    return arr.every((value, index) => index === 0 || value > arr[index - increment]);
+}
+
+const safety = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        const r = arr[i] < arr[i+1] ? arr[i+1] - arr[i] : arr[i] - arr[i+1];
+        if(r < 1 || r > 3) return false;
+    }
+
+    return true;
+}
+
+const lvls = read.map(x => x.split(' ').map(Number))
+const a1 = lvls.filter(x => isUpOrDown(x) && safety(x)).length
+// Response 524
+print(a1)
+
+// Part 2
